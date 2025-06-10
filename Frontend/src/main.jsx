539b12ee5571067ec81e6abd7a213d7ec41e058f
subin1rai <bantawasubin@gmail.com>
2025-06-10 08:58:04 +0545

    dist file

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,26 +1,31 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
 import "./index.css";
+import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
- import { RouterProvider, createRouter } from '@tanstack/react-router';
- import { routeTree } from './routing/routeTree.js';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { routeTree } from './routing/routeTree.js';
 import store from "./store/store.js";
 import { Provider } from "react-redux";
 
-
 const queryClient = new QueryClient();
-const router = createRouter({ routeTree,
+
+const router = createRouter({ 
+  routeTree,
   context: {
     queryClient,
     store
   }
+});
 
-})
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
 
-createRoot(document.getElementById("root")).render(
-  <Provider store={store }>
-
-  <QueryClientProvider client={queryClient}>
-    <RouterProvider router={router}/>    
-  </QueryClientProvider>
+createRoot(rootElement).render(
+  <Provider store={store}>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router}/>    
+    </QueryClientProvider>
   </Provider>
 );
